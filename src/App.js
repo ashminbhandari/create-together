ab77ff50a85ced754347b5386674ebb0fe67e754
ashminbhandari <abhandar@ramapo.edu>
2020-06-05 14:35:57 -0400

    cool star parallax as well as theme fixes (dark and light)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useContext, useState} from 'react';
 import './App.css';
+import { createGlobalStyle } from "styled-components";
 
 //ThemeContext at App level
 const ThemeContext = React.createContext('light');
@@ -7,15 +8,26 @@ const ThemeContext = React.createContext('light');
 function App() {
     const [theme, setTheme] = useState('light');
 
-    //Theme toggler
+    //Theme toggle
     function toggleTheme() {
-        if(theme === 'dark') setTheme('light');
-        else setTheme('dark');
+       if(theme === 'light'){
+           setTheme('dark');
+           let root = document.documentElement;
+           console.log(root.style);
+       }
+       else {
+           setTheme('light');
+       }
     }
 
     return (
         <ThemeContext.Provider value={theme}>
             <div className={`${theme} page-container`}>
+                <div id="stars-container">
+                    <div id="stars"/>
+                    <div id="stars2"/>
+                    <div id="stars3"/>
+                </div>
                 <div id="theme-toggler" onClick={toggleTheme}>
                     <i className="fa-3x far fa-lightbulb"></i>
                 </div>
---
+++ src/themes/darkNlight.js
@@ -0,0 +1,11 @@
+//Light theme
+export const lightTheme = {
+    primaryColor: 'black',
+    secondaryColor: 'white'
+}
+
+//Dark theme
+export const darkTheme = {
+    primaryColor: 'white',
+    secondaryColor: 'black'
+}
